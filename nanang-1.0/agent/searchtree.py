be00168ee63f23dc9650e38ebaf4a190a906ec15
Naufal Fikri <nauffik@gmail.com>
2019-05-04 19:22:37 +1000

    improved search tree stub

---
+++ nanang-1.0/agent/searchtree.py
@@ -0,0 +1,36 @@
+class SearchTree:
+  """
+  This class represents a standard looking search tree. Where the tree is stored
+  as a dictionary.
+
+  The most important function here is the 'next' function which returns the next
+  move that appears to be the best move after looking at the evaluation function
+  on states of the search tree. 
+  """
+
+  import game.board
+  import game.move
+
+  def __init__(self, root):
+    self._tree = {root: []}
+    self._root = root
+    self._queue = []
+  # Internal representation of the tree.
+
+  def next(self):
+    # returns the next best move to make (via a generator) should update the
+    # search tree as well.
+
+    return None 
+
+  def set_root(self, new_root):
+    # sets the root of the tree to the new root.
+    pass
+
+  def eval_move(self, move):
+    # evaluates a single move.
+    pass
+  
+  def eval_board(self, move):
+    # evaluates the board.
+    pass
