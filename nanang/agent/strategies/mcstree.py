6b4ded3840f1de6c8a69467280b0972d8697661e
Naufal Fikri <nauffik@gmail.com>
2019-05-12 15:27:50 +1000

    added full simulation code, not sure if it'll work

--- nanang/agent/strategies/mcstree.py
+++ nanang/agent/strategies/mcstree.py
@@ -17,23 +17,28 @@ class MonteCarloNode:
     self.successors = []
     self.qscore = 0
     self.nscore = 0
+    self._memoized_moves = None
   
   def explore(self, color):
-    possible_moves = self.board.possible_moves(color)
-    index = MonteCarloSearchTree.rng.randint(0, len(possible_moves) - 1)
+    if self._memoized_moves is None:
+      self._memoized_moves = self.board.possible_moves(color)
+    
+    assert(len(self._memoized_moves) > 0)
+
+    index = MonteCarloSearchTree.rng.randint(0, len(self._memoized_moves) - 1)
 
     new_node = MonteCarloNode(self, 
-      self.board.possible_board(possible_moves[index]))
+      self.board.possible_board(self._memoized_moves.pop(index)))
 
     self.successors.append(new_node)
     return new_node
     
   
-  def propagate_up(self, positive):
+  def propagate_up(self, winner):
     self.nscore += 1
-    self.qscore += int(positive)
+    self.qscore += int(self.board.current_turn == winner)
     if self.parent is not None:
-      self.parent.backtrack(positive)
+      self.parent.backtrack(winner)
 
   def node_eval(self):
     if self.nscore == 0:
@@ -73,14 +78,15 @@ class MonteCarloSearchTree(SearchTree):
   def set_root(self, new_root):
     super().set_root(new_root)
     
-    x_node = self.find_in_set(new_root)
-    if x_node is not None:
-      self._mct_root = x_node
-    else:
-      new_node = MonteCarloNode(self._mct_root, new_root)
-      self.tree_set.add(new_node)
+    # x_node = self.find_in_set(new_root)
+    # if x_node is not None:
+      # self._mct_root = x_node
+    # else:
+    self._mct_root = MonteCarloNode(self._mct_root, new_root)
+    # self.tree_set.add(new_node)
     
-    self._master_turn_count += 1
+    if (new_root.current_turn == self.color):    
+      self._master_turn_count += 1
     
 
   def find_in_set(self, board):
@@ -90,13 +96,19 @@ class MonteCarloSearchTree(SearchTree):
     return None
 
   def next_best(self):
-    pass
-  
-  def eval_node(self, node):
-    pass
-  
-  def eval_edge(self, node):
-    pass
+    self.monte_carlo_fullsearch()
+    return max(self._mct_root.successors, key=lambda n: n.nscore)
+
+  def monte_carlo_fullsearch(self):
+    for _ in range(self._sim):
+      # Selection and Expansion
+      expand = self.monte_carlo_select(self._mct_root)
+      new_node = expand.explore()
+      self.tree_set.add(new_node)
+
+      win = self.monte_carlo_simulate(new_node)
+
+      new_node.propagate_up(win)
 
   def monte_carlo_select(self, node: MonteCarloNode):
     return_node = node
@@ -126,14 +138,13 @@ class MonteCarloSearchTree(SearchTree):
       
       if board.current_turn == player_begin:
         turn_counter += 1  
-      
-    if board.get_winner() == self.color:
-      return True
-    elif board.get_winner() is None:
-      # draw mechanism, right now do a coin flip.
-      return MonteCarloSearchTree.rng.random() <= 0.3
+    
+    winner = board.get_winner()
+    if winner is not None:
+      return winner
     else:
-      return False
+      # draw mechanism, right now do a weighted three-player coin flip.
+      return "RGB"[MonteCarloSearchTree.rng.randint(0, 2)]
     
 
   
