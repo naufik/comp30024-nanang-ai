299d97c30a2b3354be719cbfcf0f3cd9c513cdc3
Unknown <fdarma@student.unimelb.edu.au>
2019-05-21 21:19:40 +1000

    removed regtree

--- pantat_bohAI/agent/strategies/regtree.py
+++
@@ -1,9 +0,0 @@
-from pantat_bohAI.agent.searchtree import SearchTree
-
-class RegTree(SearchTree):
-  """
-  A type of Learned Tree based on linear regression
-  """
-
-  def __init__(self, root, weights_file):
-    super().__init__(root)
